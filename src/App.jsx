9cb7b4022e34b8a735bb6dab5f4e70347e3e2405
hris25 <lokoharris25@gmail.com>
2025-03-11 11:59:49 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import './app.css'
+import { useEffect } from "react";
 
 const App = () => {
-  return (
-    <div>Hello World</div>
-  )
-}
+  useEffect(() => {
+    if (window.Telegram) {
+      window.Telegram.WebApp.expand();
+    }
+  }, []);
 
-export default App
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold">Mini-App Telegram</h1>
+      <p className="bg-blue-500 h-10 w-10 ">
+        Bienvenue dans ton gestionnaire de tâches 📅
+      </p>
+    </div>
+  );
+};
+
+export default App;
