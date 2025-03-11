c596b086fb1d2a33fb091e4bd5dfd64d26ec23e9
hris25 <lokoharris25@gmail.com>
2025-03-11 12:27:32 +0100

    test vercel

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,8 @@ const App = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold">Mini-App Telegram</h1>
-      <p className="bg-blue-500 h-10 w-10 ">
-        Bienvenue dans ton gestionnaire de tâches 📅
-      </p>
+      <h1 className="text-4xl text-blue-600 font-bold">Mini-App Telegram</h1>
+      <p className="bg-blue-500 h-10 w-10 ">Bienvenue</p>
     </div>
   );
 };
